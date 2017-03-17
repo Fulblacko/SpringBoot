d8a1a2875de8e1877fe9075e93b812009df77992
Sudreau-Mac <kev150313@gmail.com>
2017-03-17 11:58:43 +0100

    Add security system

---
+++ src/main/java/fr/polytech/web/LoginController.java
@@ -0,0 +1,15 @@
+package fr.polytech.web;
+/**
+ * Polytech Marseille
+ * Case 925 - 163, avenue de Luminy
+ * 13288 Marseille CEDEX 9
+ * <p>
+ * Ce fichier est l'oeuvre d'eleves de Polytech Marseille. Il ne peut etre
+ * reproduit, utilise ou modifie sans l'avis express de ses auteurs.
+ */
+
+/**
+ * @author Sudreau
+ */
+public class LoginController {
+}
